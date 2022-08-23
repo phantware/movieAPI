d26b925fe4c2bf96cd065bb4970e29c5b1075aac
phantware <phantmoney2011@gmail.com>
2022-08-23 20:56:57 +0100

    MovieSchema created

--- models/User.js
+++ models/User.js
@@ -2,24 +2,10 @@ import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    profilePic: {
-      type: String,
-      default: '',
-    },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, default: '' },
     isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }

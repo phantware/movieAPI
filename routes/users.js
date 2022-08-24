fd7a4cf2b09026acb3617cac28f1d0ce909381a5
phantware <phantmoney2011@gmail.com>
2022-08-24 17:51:25 +0100

    Delete route implemented

--- routes/users.js
+++ routes/users.js
@@ -29,6 +29,20 @@ router.put('/:id', verify, async (req, res) => {
   }
 })
 
+//DELETE
+
+router.delete('/:id', verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id)
+      return res.status(200).json('User has been deleted...')
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(403).json('You can delete only your account')
+  }
+})
 router.get('/', async (req, res) => {
   const users = await User.findAll()
 

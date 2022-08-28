9691edfb33f08388aa2ba23d9fac6ecd805707ec
phantware <phantmoney2011@gmail.com>
2022-08-28 14:57:29 +0100

    list api page completed

--- routes/lists.js
+++ routes/lists.js
@@ -56,6 +56,7 @@ router.get('/', verify, async (req, res) => {
     } else {
       list = await List.aggregate([{ $sample: { size: 10 } }])
     }
+    return res.statu(200).json(list)
   } catch (err) {
     return res.status(500).json(err)
   }

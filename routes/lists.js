9b07e312a1888c9caf3695f49f36e76b24274cad
phantware <phantmoney2011@gmail.com>
2022-08-28 14:03:58 +0100

    movieapi updated

--- routes/lists.js
+++ routes/lists.js
@@ -1,3 +1,4 @@
+const { aggregate } = require('../models/List')
 const List = require('../models/List')
 const verify = require('../verifyToken')
 const router = require('express').Router()
@@ -30,6 +31,33 @@ router.delete('/:id', verify, async (req, res) => {
     }
   } else {
     return res.status(403).json('You are not allowed!')
+  }
+})
+
+//GET
+
+router.get('/', verify, async (req, res) => {
+  const typeQuery = req.query.type
+  const genryQuery = req.query.genry
+  let list = []
+  try {
+    if (typeQuery) {
+      if (genryQuery) {
+        list = await aggregate([
+          { $sample: { size: 10 } },
+          { $math: { type: typeQuery, genre: genryQuery } },
+        ])
+      } else {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $math: { type: typeQuery } },
+        ])
+      }
+    } else {
+      list = await List.aggregate([{ $sample: { size: 10 } }])
+    }
+  } catch (err) {
+    return res.status(500).json(err)
   }
 })
 module.exports = router
